76a9010251d18c5c342e38b12fba696352c2523b
BrehamPie <44113050+BrehamPie@users.noreply.github.com>
2020-08-27 23:09:27 +0600

    Update DSU.cpp

--- Data Structure/DSU.cpp
+++ Data Structure/DSU.cpp
@@ -31,33 +31,4 @@ int main()
     int n;
     cin>>n;
     DSU dsu(n);
-    vector<int>ruts;
-    vector<pii>dlt;
-    for(int i=1;i<n;i++)
-    {
-        int u,v;
-        int p=dsu.marge(u,v);
-        if(p+1) continue;
-        dlt.pb({u,v});
-    }
-    bool vis[n+1];
-    for(int i=1;i<=n;i++)
-    {
-        int u=dsu.root(i);
-        if(vis[u]) continue;
-        ruts.pb(u);
-    }
-    vector<pii>ans;
-    while(dsu.components()>1)
-    {
-        ans.pb({dlt.back().ff,dlt.back().ss});
-        dlt.pop_back();
-        int u=ruts.back();
-        ruts.pop_back();
-        int v=ruts.back();
-        int x=dsu.marge(u,v);
-        ruts.pb(x);
-    }
-    cout<<ans.size()/2<<endl;
-    for(int i=0;i<ans.size();i++) cout<<ans[i].ff<<' '<<ans[i].ss<<(i%2?'\n':' ');
 }
