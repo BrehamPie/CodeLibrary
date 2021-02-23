aec0e4334d0b07594f3c4342bb5b3de11fead39e
BrehamPie <44113050+BrehamPie@users.noreply.github.com>
2021-02-23 19:43:29 +0600

    Create Gauss(modulo2).cpp

---
+++ Math/Gauss(modulo2).cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+template<typename T>
+struct Gauss
+{
+    int bits  = 60;
+    vector<T>table;
+    Gauss()
+    {
+        table  = vector<T>(bits,0);
+    }
+    Gauss(int _bits)
+    {
+        bits = _bits;
+        table = vector<T>(bits,0);
+    }
+    int basis()//return rank/size of basis
+    {
+        int ans = 0;
+        for(int i=0;i<bits;i++)
+            if(table[i])
+                ans++;
+        return ans;
+    }
+    bool can(T x)//can x be obtained from the basis
+    {
+        for(int i=bits-1;i>=0;i--) x=min(x,x^table[i]);
+        return x==0;
+    }
+    void add(T x)
+    {
+        for(int i=bits-1;i>=0 && x;i--)
+        {
+            if(table[i]==0)
+            {
+                table[i] = x;
+                x = 0;
+            }
+            else x=min(x,x^table[i]);
+        }
+    }
+    T getBest()
+    {
+        T x = 0;
+        for(int i=bits-1;i>=0;i--)
+            x = max(x,x^table[i]);
+        return x;
+    }
+    void Merge(Gauss &other)
+    {
+        for(int i=bits-1;i>=0;i--) add(other.table[i]);
+    }
+};
+int main()
+{   
+    //call with constructor to define bit size.
+    Gauss<long long>gs;
+}
